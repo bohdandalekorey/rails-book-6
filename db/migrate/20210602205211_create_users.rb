c9f000ec7b1c8e49e7fdde3bd3406156a4abaffb
shewlakov <oleksandrsevlakov@.gmail.com>
2021-06-03 23:15:44 +0300

    Add validation of User

--- db/migrate/20210602205211_create_users.rb
+++ db/migrate/20210602205211_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[6.1]
     create_table :users do |t|
       t.string :name
       t.string :email
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end
