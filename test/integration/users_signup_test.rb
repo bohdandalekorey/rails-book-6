ab7398d0b5e43f379af831ad0597a0e751412104
shewlakov <oleksandrsevlakov@.gmail.com>
2021-06-08 22:52:00 +0300

    Fix syntax mistake

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -7,6 +7,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_no_difference 'User.count' do
       post users_path, user: { name: '', email: 'user@invalid', password: 'foo', password_confirmation: 'bar' }
     end
+
     assert_template 'users/new'
   end
 
@@ -16,6 +17,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_difference 'User.count', 1 do
       post_via_redirect users_path, user: { name: 'Example User', email: 'user@example.com', password: 'password', password_confirmation: 'password' }
     end
+
     assert_template 'users/show'
   end
 end
