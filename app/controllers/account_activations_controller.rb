100281b00252857ee31410666a8928db73b75a71
shewlakov <oleksandrsevlakov@.gmail.com>
2021-06-21 22:37:20 +0300

    Fix syntax mistake

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -5,11 +5,10 @@ class AccountActivationsController < ApplicationController
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
       user.activate
       log_in user
-      flash[:success] = "Account a1ctivated!"
+      flash[:success] = 'Account a1ctivated!'
       redirect_to user
-
     else
-      flash[:danger] = "Invalid activation link"
+      flash[:danger] = 'Invalid activation link'
       redirect_to root_url
     end
   end
