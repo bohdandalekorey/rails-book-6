9d1faede4c5f71e93ef98be1b3f66302f91837b9
shewlakov <oleksandrsevlakov@.gmail.com>
2021-06-22 17:35:40 +0300

    Fix database mistake

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2021_06_21_154442) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,7 +22,6 @@ ActiveRecord::Schema.define(version: 2021_06_21_154442) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin", default: false
-    t.string "s"
     t.string "activation_digest"
     t.boolean "activated", default: false
     t.datetime "activated_at"
@@ -31,5 +29,4 @@ ActiveRecord::Schema.define(version: 2021_06_21_154442) do
     t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
-
 end
