c4fbdabc53aee145a58be61ba672d7a3640bc522
shewlakov <oleksandrsevlakov@.gmail.com>
2021-06-23 21:00:45 +0300

    Add Micropost model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   attr_accessor :remember_token, :activation_token, :reset_token
 
   has_secure_password
+  has_many :microposts, dependent: :destroy
 
   before_save :downcase_email
   before_save { self.email = email.downcase }
